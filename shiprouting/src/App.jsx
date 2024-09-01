8ce9dfa44a914784d02475a8891b480aa725dede
Siddhesh179 <siddhesh8@outlook.com>
2024-09-01 22:12:48 +0530

    Map components

---
+++ shiprouting/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import MapComponent from "./components/Map";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Indian Ocean Map</h1>
+      <MapComponent />
+    </div>
+  );
+}
+
+export default App;
